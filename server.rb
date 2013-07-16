1f416402c9fb1024767ecd8b439c7d37e5469613
Garen Torikian <gjtorikian@gmail.com>
2013-07-16 16:31:44 -0700

    Create server.rb

---
+++ server.rb
@@ -0,0 +1,22 @@
+var express = require("express"),
+    app     = express(),
+    port    = 8888;
+    
+app.get("/", function(req, res) {
+  res.redirect("index.html");
+});
+
+app.configure(function(){
+  app.use(express.methodOverride());
+  app.use(express.bodyParser());
+  app.use(express.static(__dirname));
+  app.use(express.errorHandler({
+    dumpExceptions: true, 
+    showStack: true
+  }));
+  app.use(app.router);
+});
+
+app.listen(port);
+
+console.log("Server started at localhost:8888");
